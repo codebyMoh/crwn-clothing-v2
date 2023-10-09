31a45787fc4ed02c1bff59f464f29f7c28f47420
“codebymoh” <codebymohit@gmail.com>
2023-10-09 13:15:25 +0530

    routes set for navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, } from 'react-router-dom';
 import Home from './routes/home/home.component';
+import Navigation from './routes/Navigation/navigation.component';
 
+
+const Shop = () => {
+
+  return (
+    <h1> I am Shop Page</h1>
+  )
+}
 
 const App = () => {
 
   return (
     <Routes>
+      <Route path='/' element={<Navigation />} >
 
-      <Route path='/' element={<Home />}>
-      </Route>
+      <Route index element={<Home />} />
+
+      <Route path='shop' element={<Shop />} />
+
+        </Route>
+     
     
     </Routes>
   );
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,3 +1,4 @@
+import {Outlet} from 'react-router-dom';
 import Directory from '../../components/directory/directory.component';
 
 
@@ -32,7 +33,12 @@ const Home = () => {
     ];
 
 
-    return <Directory categories={categories} />;
+    return (
+    <div>
+<Outlet/>
+<Directory categories={categories} />;
+    </div>
+    )
   
 
 };
